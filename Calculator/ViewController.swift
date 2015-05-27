1d7783e1b81ee6ae902fea0616d8ac1f4907c992
Liam Hamill <hamilllr@gmail.com>
2015-05-27 11:12:55 +0100

    First commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,117 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    var isTyping: Bool = false
+    let pi = 22.0/7
+    
+    let defaultDisplay = "0"
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if isTyping {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            isTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func displayPi(sender: UIButton) {
+        
+        enter()
+        isTyping = true
+        display.text = "\(pi)"
+        
+        return
     }
+    
+    private func clear() {
+        display.text = defaultDisplay
+        brain.emptyStack()
+        isTyping = false
+    }
+    
 
-
+    @IBAction func clearB(sender: UIButton) {
+        clear()
+    }
+    
+    @IBAction func deleteButton() {
+        let len = count(display.text!)
+        let reset = len <= 1
+        
+        if reset {
+            clear()
+            return
+        } else {
+            let index: String.Index = advance(display.text!.startIndex, len - 1)
+            display.text = display.text!.substringToIndex(index)
+        }
+    }
+    
+    
+    @IBAction func period(sender: UIButton) {
+        let period = sender.currentTitle!
+        
+        for char in display.text! {
+            if char == "." { return }
+        }
+        
+        if isTyping {
+            display.text! += period
+        } else {
+            display.text = "0" + period
+            isTyping = true
+        }
+        
+    }
+    
+    @IBAction func enter() {
+        
+        isTyping = false
+        
+        if let result = brain.pushOperand(displayValue!) {
+            displayValue = result
+        }
+    }
+    
+    var displayValue: Double? {
+        get {
+            if let returnVal = display.text {
+                
+                return (returnVal as NSString).doubleValue
+                
+            } else {
+                clear()
+                return nil }
+        }
+        
+        set {
+            if floor(newValue!) == newValue! {
+                display.text = "\(Int(newValue!))"
+            } else {
+                display.text = "\(newValue!)"
+            }
+        }
+    }
+    @IBAction func operate(sender: UIButton) {
+        
+        let opSymbol = sender.currentTitle!
+        
+        if isTyping {
+            enter()
+        }
+        
+        if let output = brain.performOperation(opSymbol) {
+            displayValue = output
+        }
+        
+        
+    }
+    
 }
 
